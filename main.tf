d89f6ae5e2a435b4399230a6c8b079de1bbc1f7e
yaochaoutokyo@gmail.com <yaochaoutokyo@gmail.com>
2020-07-06 20:27:13 +0900

    Initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider aws {
+  region = "ap-northeast-1"
+  profile = "yann"
+  version = ">= 2.28.1"
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
